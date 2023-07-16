3a29f66146118e3451e88fd8731e7fb1b0680077
JEHM94 <104336128+JEHM94@users.noreply.github.com>
2023-07-16 19:34:10 -0400

    Mejora la vista de cada producto en dispositivos móviles

--- react-freshcoffee/src/components/Producto.jsx
+++ react-freshcoffee/src/components/Producto.jsx
@@ -9,50 +9,51 @@ export default function Producto({ producto, botonAgregar = false, botonDisponib
   const { handleClickModal, handleSetProducto, handleClickProductoAgotado } = useQuiosco();
 
   return (
-    <div className="border p-3 shadow bg-white">
+    <div className="border p-3 shadow bg-white flex flex-col">
       {/* <img
         src={`/img/${imagen}.jpg`}
         alt={`Imagen de ${nombre}`}
         className="w-full m-auto rounded-md"
       /> */}
-      <div className="w-full m-auto rounded-md">
+      <div className="w-full rounded-md">
         <picture >
           <source srcSet={`/img/${imagen}.webp`} type="image/webp" />
           <img src={`/img/${imagen}.jpg`} alt={`Imagen de ${nombre}`} loading="lazy" />
         </picture>
       </div>
 
-      
-      <div className="p-5">
+
+      <div className="pt-2 md:p-5 h-full flex flex-col justify-between">
         <h3 className="text-xl font-bold">
           {nombre}
         </h3>
 
-        <p className="mt-5 font-black text-4xl text-amber-500">
-          {formatearDinero(precio)}
-        </p>
+        <div>
+          <p className="mt-5 font-black text-4xl text-amber-500">
+            {formatearDinero(precio)}
+          </p>
 
-        {botonAgregar && (
-          <button
-            type="button"
-            onClick={() => {
-              handleClickModal()
-              handleSetProducto(producto)
-            }}
-            className="bg-indigo-600 hover:bg-indigo-700 text-white w-full mt-5 p-3 uppercase font-bold rounded"
-          >Agregar
-          </button>
-        )}
+          {botonAgregar && (
+            <button
+              type="button"
+              onClick={() => {
+                handleClickModal()
+                handleSetProducto(producto)
+              }}
+              className="bg-indigo-600 hover:bg-indigo-700 text-white text-xs md:text-base w-full mt-5 p-3 uppercase font-bold rounded"
+            >Agregar
+            </button>
+          )}
 
-        {botonDisponible && (
-          <button
-            type="button"
-            onClick={() => handleClickProductoAgotado(producto.id, producto.nombre, disponible)}
-            className={`${disponible ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-red-600 hover:bg-red-700'} text-white w-full mt-5 p-3 uppercase font-bold rounded`}
-          >{disponible ? 'Producto Disponible' : 'Producto Agotado'}
-          </button>
-        )}
-
+          {botonDisponible && (
+            <button
+              type="button"
+              onClick={() => handleClickProductoAgotado(producto.id, producto.nombre, disponible)}
+              className={`${disponible ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-red-600 hover:bg-red-700'} text-white text-xs md:text-base w-full mt-5 p-3 uppercase font-bold rounded`}
+            >{disponible ? 'Producto Disponible' : 'Producto Agotado'}
+            </button>
+          )}
+        </div>
       </div>
 
     </div>
